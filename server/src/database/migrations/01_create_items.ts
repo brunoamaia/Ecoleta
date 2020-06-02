b1eb55b0079208e98b5b1160589ced7fe9991748
brunoamaia <brunoamaia94@gmail.com>
2020-06-02 11:23:10 -0300

    tabelas e migrations

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -1,3 +1,13 @@
-export async function up() {}
+import Knex from 'knex';
 
-export async function down() {}
+export async function up(knex: Knex) {  // forma de utilizar o Typescript para ajudar
+    return knex.schema.createTable('items', table => {
+        table.increments('id').primary();   // coluna de identificação e com autoincremento
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('items')
+}
--- server/src/database/migrations/02_create_pint_items.ts
+++ server/src/database/migrations/02_create_pint_items.ts
@@ -1,3 +1,17 @@
-export async function up() {}
+import Knex from 'knex';
 
-export async function down() {}
+export async function up(knex: Knex) {  // forma de utilizar o Typescript para ajudar
+    return knex.schema.createTable('point_items', table => {
+        table.increments('id').primary();   // coluna de identificação e com autoincremento
+
+        table.integer('point_id').notNullable()
+        .references('id').inTable('points');    // Chave estrangeira (precisa ser um ID válido da tabela points)
+
+        table.integer('item_id').notNullable()
+        .references('id').inTable('items');
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_items')
+}
