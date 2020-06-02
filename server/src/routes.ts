4a4df87c8b48fe1b20a5710591a00661ab2db32a
brunoamaia <brunoamaia94@gmail.com>
2020-06-02 12:09:06 -0300

    mostrar a rota da mimagem

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {   // Home
-    return response.json({ message: 'Hello World' });
+routes.get('/items', async (request, response) => {   // Consultar itens
+    const items = await knex('items').select('*');    // Seleciona todos os elelementos da tabela "itens"
+
+    const serializedItems = items.map( item => { // Percorre todos os itens e permite modificá-los
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });      
+    return response.json(serializedItems);
 });
 
 export default routes;  // Precisa exportar, para que seja importada
+
+/*
+routes.get('/', (request, response) => {   // Home
+    return response.json({ message: 'Hello World' });
+});
+*/
