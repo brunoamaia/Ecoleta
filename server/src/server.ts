2e3028e5916129ab6bcbc0f4e3e3f9217311c8a6
brunoamaia <brunoamaia94@gmail.com>
2020-06-02 09:52:04 -0300

    get, post, put, request

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,38 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
-const app = express();
+const app = express(); // Por padrão não entende o Json
+app.use(express.json());
+
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+];
 
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+    const search = String(request.query.search);
 
-    //response.send('Hello World')
-    response.json([
-        'Diego',
-        'Cleitom',
-        'Robson'
-    ])
+    const filteredUsers = search ? users.filter(user =>user.includes(search)): users; // if ternário
+
+    response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+    return response.json(user);
 });
 
 app.listen(3333); 
